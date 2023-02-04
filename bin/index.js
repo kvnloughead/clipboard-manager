3bfe2aa7fadbb18b75b128b928bb41a5c17fcf1b
Kevin Loughead <kvnloughead@gmail.com>
2023-02-03 23:38:58 -0500

    Add `save` command

--- bin/index.js
+++ bin/index.js
@@ -1,25 +1,29 @@
 #!/usr/bin/env node
 
-import clipboard from "clipboardy";
-import fs from "fs";
-import _yargs from "yargs";
-import { hideBin } from "yargs/helpers";
+import _yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 const yargs = _yargs(hideBin(process.argv));
 
-const dataFile = "/home/kevin/Dropbox/cb/clips.json";
-const data = JSON.parse(fs.readFileSync(dataFile));
-import save from "../commands/save.js";
+import save from '../commands/save.js';
 
-yargs.command(
-  "save [key]",
-  'save contents of clipboard with "key" as its hash',
-  (yargs) => {
-    yargs.positional("key", {
-      describe: "key to associate with clipboard contents",
-      default: 0,
-    });
-  },
-  save
-).argv;
-
-console.log(clipboard.readSync());
+yargs
+  .command(
+    ['save [key]', 's'],
+    'save contents of clipboard with "key" as its hash',
+    (yargs) => {
+      yargs.positional('key', {
+        describe: 'key to associate with clipboard contents',
+        default: 0,
+      });
+    },
+    save,
+  )
+  .option('verbose', {
+    alias: 'v',
+    type: 'boolean',
+    description: 'run with verbose logging',
+  })
+  .demandCommand()
+  .showHelpOnFail(true)
+  .help('h')
+  .alias('h', 'help').argv;
--- commands/save.js
+++ commands/save.js
@@ -1,5 +1,12 @@
-function save(argv) {
-  console.log(argv);
+import fs from "fs";
+import clipboard from "clipboardy";
+
+const dataFile = "/home/kevin/Dropbox/cb/clips.json";
+
+function save({ key }) {
+  const data = JSON.parse(fs.readFileSync(dataFile));
+  data[key] = clipboard.readSync();
+  fs.writeFileSync(dataFile, JSON.stringify(data, null, 2));
 }
 
 export default save;
