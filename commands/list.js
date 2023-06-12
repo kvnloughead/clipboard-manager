c3c732b83413406ba54daa795116e3ddde366632
Kevin Loughead <kvnloughead@gmail.com>
2023-06-11 23:44:35 -0400

    Add Option class to support custom messages on subcommands better

--- commands/list.js
+++ commands/list.js
@@ -1,6 +1,10 @@
 import fs from 'fs';
 
-import { printTableFromObject, truncateString } from '../utils/helpers.js';
+import {
+  listImages,
+  printTableFromObject,
+  truncateString,
+} from '../utils/helpers.js';
 
 /**
  * Lists all key/value pairs in clips file. Can be ugly or less ugly.
@@ -10,17 +14,22 @@ import { printTableFromObject, truncateString } from '../utils/helpers.js';
  * @param {string} argv.pretty - option for kind of pretty printing -p|--pretty
  *
  */
-function list({ file, pretty }) {
+function list(args) {
+  const { file, pretty, verbose, imagesPath } = args;
+  if (args.img) {
+    console.log(listImages(imagesPath).join('\n'));
+    return;
+  }
   const data = JSON.parse(fs.readFileSync(file));
   const maxKeyLength = Math.max(...Object.keys(data).map((k) => k.length));
   const { columns } = process.stdout; // when piping, this will be undefined
   if (pretty) {
     printTableFromObject(data, columns, { key: maxKeyLength });
-  } else {
+  } else if (verbose) {
     let res = `\n`;
     Object.entries(data).forEach(([k, v]) => {
       const key = k.padStart(maxKeyLength, ' ');
-      let val = v.replace(/\s{2,}/g, '\n').trim();
+      let val = v.replace(/\n/g, '\\n').trim();
       // TODO - figure out how to determine terminal width when piping
       val = columns
         ? truncateString(val, columns / 2, { ellipsis: false })
@@ -28,6 +37,10 @@ function list({ file, pretty }) {
       res += `${key}\t${val}\n`;
     });
     console.log(res + `\n`);
+  } else {
+    Object.entries(data).forEach(([k, v]) => {
+      console.log(k);
+    });
   }
 }
 
