27f3e6038208746c4fb638ec5698d08ad2afeb33
Kevin Loughead <kvnloughead@gmail.com>
2023-10-12 21:41:00 -0400

    feat: commands to stop and restart the tracker

--- commands/tracker.js
+++ commands/tracker.js
@@ -1,17 +1,55 @@
 import { spawn } from "child_process";
+import fs from "fs";
+import path from "path";
 
 function tracker(args) {
-  const trackerPath = new URL("../utils/tracker.js", import.meta.url).pathname;
+  const trackerPidPath = path.join(args.logsPath, "tracker.pid");
 
-  const child = spawn("node", [trackerPath, JSON.stringify(args)], {
-    detached: true,
-    setsid: true,
-    stdio: ["ignore", "inherit", "inherit"],
-  });
+  function start() {
+    const trackerPath = new URL("../utils/tracker.js", import.meta.url)
+      .pathname;
 
-  console.log("Started tracking clipboard in background.");
-  child.unref();
-  process.exit(0);
+    const child = spawn("node", [trackerPath, JSON.stringify(args)], {
+      detached: true,
+      setsid: true,
+      stdio: ["ignore", "inherit", "inherit"],
+    });
+
+    // save process pid for easy stopping
+    fs.writeFileSync(trackerPidPath, child.pid.toString());
+
+    console.log("Started tracking clipboard in background.");
+    child.unref();
+    process.exit(0);
+  }
+
+  function stop() {
+    try {
+      const pid = parseInt(fs.readFileSync(trackerPidPath, "utf-8"));
+      process.kill(pid);
+      fs.writeFileSync(trackerPidPath, "");
+      console.log("Stopped tracking clipboard.");
+    } catch (err) {
+      console.error(`Can't stop tracking clipboard, no process found.`);
+      (args.verbose || args.debug) && console.error(err);
+    }
+  }
+
+  function restart() {
+    const pid = parseInt(fs.readFileSync(trackerPidPath, "utf-8"));
+    if (Number.isInteger(pid)) {
+      stop();
+    }
+    start();
+  }
+
+  if (args.action === "start") {
+    start();
+  } else if (args.action === "stop") {
+    stop();
+  } else if (args.action === "restart") {
+    restart();
+  }
 }
 
 export default tracker;
