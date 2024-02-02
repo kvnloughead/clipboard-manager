dfbc37473e0132f32e898760fe24b67f52490202
Kevin Loughead <kvnloughead@gmail.com>
2024-02-01 20:59:27 -0500

    fix: path output when setting images

--- middleware/index.ts
+++ middleware/index.ts
@@ -1,14 +1,28 @@
 import _yargs, { Arguments } from "yargs";
 import { appLogger } from "../utils/logger.js";
 
-export const setFilePath = (argv: Arguments) => {
-  // If argv.config, then commands should be run on the config file instead of
-  // clips file. Not fully implemented.
-  argv.file = argv.config ? argv.configFile : argv.clipsFile;
+/**
+ * Sets the filepath for use in command handlers. There are the following
+ * cases:
+ *
+ *  - If args.config is true, then args.file will be the configFile path
+ *  - If args.img is true, then args.file will be the imagesPath directory
+ *  - Otherwise, args.file will be the clipsFile.
+ *
+ * @param args - an object with CLI args and user settings.
+ */
+export const setFilePath = (args: Arguments) => {
+  if (args.config) {
+    args.file = args.configFile;
+  } else if (args.img) {
+    args.file = args.imagesPath;
+  } else {
+    args.file = args.clipsFile;
+  }
 };
 
-export const debug = (argv: Arguments) => {
-  if (argv.debug) {
+export const debug = (args: Arguments) => {
+  if (args.debug) {
     appLogger.debug();
   }
 };
